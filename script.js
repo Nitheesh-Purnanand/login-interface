5bfaeb423c2925e3e1b4caf0b54a10926295ab2d
Nitheesh Ch <chalumuri.nitheesh@gmail.com>
2024-07-06 23:06:29 +0530

    some what working

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".bubble-wrapper");
+const animatebubble = x =>{
+    const bubble = document.createElement("div");
+    bubble.className = "bubble";
+    bubble.style.left = `${x}px`;
+    wrapper.appendChild(bubble);
+    setTimeout(() => {
+        wrapper.removeChild(bubble);
+    }, 2000);
+}
+window.onmousemove = e => animatebubble(e.clientX);
+
+// const wrapper = document.querySelector(".bubble-wrapper");
+
+// const animateBubble = x => {  
+//   const bubble = document.createElement("div");
+  
+//   bubble.className = "bubble";
+//   bubble.style.left = `${x}px`;
+  
+//   wrapper.appendChild(bubble);
+  
+//   setTimeout(() => wrapper.removeChild(bubble), 2000);
+// }
+
+// window.onmousemove = e => animateBubble(e.clientX);
